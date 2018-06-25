7f31bedfa4cf5c5ad79251fdca2b558ec29e0eb2
Jake M Reifer <reifer.jake@gmail.com>
2018-06-25 11:29:21 -0700

    First Steps

---
+++ app.js
@@ -0,0 +1,67 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const botbuilder_1 = require("botbuilder");
+const restify = require("restify");
+const { MessageFactory } = require('botbuilder');
+// Create server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`${server.name} listening to ${server.url}`);
+});
+// Create adapter
+const adapter = new botbuilder_1.BotFrameworkAdapter({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+// Add state middleware
+const storage = new botbuilder_1.MemoryStorage();
+const convoState = new botbuilder_1.ConversationState(storage);
+const userState = new botbuilder_1.UserState(storage);
+// Add conversation state middleware
+const conversationState = new botbuilder_1.ConversationState(new botbuilder_1.MemoryStorage());
+adapter.use(conversationState);
+function Welcome(context) {
+    if (context.activity.type === 'conversationUpdate' && context.activity.membersAdded[0].name !== 'Bot') {
+        return true;
+    }
+    return false;
+}
+function responseCheck(text) {
+    switch (text) {
+        case 'FAQs':
+            return true;
+        case 'Band Search':
+            return true;
+        case 'Navigate':
+            return true;
+        default:
+            return false;
+    }
+}
+const questionMessage = MessageFactory.suggestedActions(['red', 'green', 'blue'], 'How would you like to explore the event?');
+const welcomeMessage = "Hey there! I'm the ASH Music Festival Bot. I'm here to guide you around the festival!";
+// Listen for incoming requests 
+server.post('/api/messages', (req, res) => {
+    // Route received request to adapter for processing
+    adapter.processActivity(req, res, (context) => __awaiter(this, void 0, void 0, function* () {
+        const state = conversationState.get(context);
+        if (Welcome(context)) {
+            yield context.sendActivity(welcomeMessage);
+            yield context.sendActivity(questionMessage);
+        }
+        if (context.activity.type === 'message') {
+            var response = context.activity.text;
+            if (responseCheck(response)) {
+                yield context.sendActivity(`You clicked the ${response} button!`);
+            }
+        }
+    }));
+});
---
+++ app.ts
@@ -0,0 +1,71 @@
+import { BotFrameworkAdapter, MemoryStorage, UserState, ConversationState, TurnContext } from 'botbuilder';
+import * as restify from 'restify';
+import { STATUS_CODES } from 'http';
+const {MessageFactory} = require('botbuilder');
+
+// Create server
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`${server.name} listening to ${server.url}`);
+});
+
+// Create adapter
+const adapter = new BotFrameworkAdapter({ 
+    appId: process.env.MICROSOFT_APP_ID, 
+    appPassword: process.env.MICROSOFT_APP_PASSWORD 
+});
+
+// Define conversation state shape
+interface C1State {
+    count: number;
+}
+
+// Add state middleware
+const storage = new MemoryStorage();
+const convoState = new ConversationState(storage);
+const userState = new UserState(storage);
+
+// Add conversation state middleware
+const conversationState = new ConversationState<C1State>(new MemoryStorage());
+adapter.use(conversationState);
+
+function Welcome(context: TurnContext) : boolean {
+    if (context.activity.type === 'conversationUpdate' && context.activity.membersAdded[0].name !== 'Bot') {
+        return true;
+    }
+    return false;
+}
+
+function responseCheck(text: string) : boolean {
+    switch(text) {
+        case 'FAQs':
+            return true;
+        case 'Band Search':
+            return true;
+        case 'Navigate':
+            return true;
+        default:
+            return false;
+    }
+}
+const questionMessage = MessageFactory.suggestedActions(['red', 'green', 'blue'], 'How would you like to explore the event?');
+const welcomeMessage = "Hey there! I'm the ASH Music Festival Bot. I'm here to guide you around the festival!";
+
+
+// Listen for incoming requests 
+server.post('/api/messages', (req, res) => {
+    // Route received request to adapter for processing
+    adapter.processActivity(req, res, async (context) => {
+        const state = conversationState.get(context);
+        if(Welcome(context)) {
+            await context.sendActivity(welcomeMessage);
+            await context.sendActivity(questionMessage);
+        }
+        if (context.activity.type === 'message') {
+            var response = context.activity.text;
+            if (responseCheck(response)) {
+                await context.sendActivity(`You clicked the ${response} button!`)
+            }
+        }
+    });
+});
