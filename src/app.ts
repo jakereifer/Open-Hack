1f9d1270239b23487d21bb5f1abd9d78d915bf1f
Jake M Reifer <reifer.jake@gmail.com>
2018-07-02 15:56:55 -0700

    Finished Challenge 4

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { BotFrameworkAdapter, MemoryStorage, UserState, ConversationState, TurnContext } from 'botbuilder';
+import { BotFrameworkAdapter, MemoryStorage, UserState, ConversationState, TurnContext, ActionTypes } from 'botbuilder';
 import * as restify from 'restify';
 import { STATUS_CODES } from 'http';
 import { Context, isContext } from 'vm';
@@ -64,15 +64,15 @@ function refreshBot(state: C1State): void {
     state.questionText = '';
     state.answerText = '';
     state.correctAnswer = false;
-    state.menuFlag = false;
-    state.FAQFlag = false;
-    state.questionFlag = false;
-    state.answerFlag = false;
+    state.currentBands = [];
+    state.navDay = '';
+    state.navGenre = '';
 }
 
 const questionMessage = MessageFactory.suggestedActions(['FAQs', 'Band Search', 'Navigate'], 'How would you like to explore the event?');
 const welcomeMessage = "Hey there! I'm the ASH Music Festival Bot. I'm here to guide you around the festival!";
-const confirmMessage = MessageFactory.suggestedActions(['Yes', 'No'], 'Was this the answer you were looking for?')
+const confirmMessage = MessageFactory.suggestedActions(['Yes', 'No'], 'Was this the answer you were looking for?');
+
 
 function addFAQ(q: string, a: string, gr: boolean) {
     var storageEntity = {
@@ -95,11 +95,8 @@ interface C1State {
     answerText: string;
     correctAnswer: boolean;
     currentBands: Band[];
-    //flags
-    menuFlag: boolean;
-    FAQFlag: boolean;
-    questionFlag: boolean;
-    answerFlag: boolean;
+    navDay: string;
+    navGenre: string;
 }
 const qnaURL: string = "https://openhackqnamaker.azurewebsites.net/qnamaker/knowledgebases/df82db7b-3e22-4d25-9e27-9055d64b6b8c/generateAnswer";
 
@@ -125,7 +122,18 @@ const questionURL = 'https://openhackqnamaker-asbj2jawqzgtejk.search.windows.net
 
 interface SearchAnswer {
     "@odata.context": string;
+    "@search.facets"?: DGFacets;
     value: Band[];
+}
+interface DGFacets {
+    "day@data.type"?: string;
+    "day"?: Facet[];
+    "genre@data.type"?: string;
+    "genre"?: Facet[];
+}
+interface Facet {
+    'count': number;
+    'value': string;
 }
 interface Band {
     "@search.score": string,
@@ -141,25 +149,80 @@ interface Band {
     "date": string
 }
 
-async function bandSearch(band : string): Promise<Band[]> {
-    var bandlist: SearchAnswer = await fetch(questionURL+band, {
+//filter=day%20eq%20'Sunday'%20and%20genre%20eq%20'Rock'
+async function navSearch(day: string, genre: string): Promise<Band[]> {
+    var suffix: string;
+    if (day === 'Any' && genre === 'Any') {
+        suffix = '';
+    }
+    else if (day === 'Any') {
+        suffix = `&%24filter=genre%20eq%20'${genre}'`;
+    }
+    else if (genre === 'Any') {
+        suffix = `&%24filter=day%20eq%20'${day}'`;
+    }
+    else {
+        suffix = `&%24filter=day%20eq%20'${day}'%20and%20genre%20eq%20'${genre}'`;
+    }   
+    var navSearchURL : string = `https://openhackqnamaker-asbj2jawqzgtejk.search.windows.net/indexes/my-target-index/docs?api-version=2016-09-01&search=&%24`
+    var bandlist: SearchAnswer = await fetch(navSearchURL+suffix, {
         method: 'GET',
         headers: {
             'api-key': "D4BD28224DB0862A9819C003C5D90F5B"
         },
         body:'',
-    })
+        })
         .then(function (response) {
             return response.json();
         });
     return Promise.resolve(bandlist.value);
 }
 
+async function bandSearch(band : string): Promise<Band[]> {
+    var bandlist: SearchAnswer = await fetch(questionURL+band/*+'~&queryType=full&facet=day'*/, {
+        method: 'GET',
+        headers: {
+            'api-key': "D4BD28224DB0862A9819C003C5D90F5B"
+        },
+        body:'',
+        })
+        .then(function (response) {
+            return response.json();
+        });
+    return Promise.resolve(bandlist.value);
+}
 
+function findPictureURL(img : string) : string {
+    return 'https://ashbotnodev49e75.blob.core.windows.net/band-pics/' + img.substring(9);
+}
 
+async function daySearch() : Promise<DGFacets> {
+    var SA : SearchAnswer = await fetch(questionURL+'*&facet=day', {
+            method: 'GET',
+            headers: {
+                'api-key': "D4BD28224DB0862A9819C003C5D90F5B"
+            },
+            body:'',
+            })
+            .then(function (response) {
+                return response.json();
+            });
+    return Promise.resolve(SA["@search.facets"]);
+}
 
-
-
+async function genreSearch() : Promise<DGFacets> {
+    var SA : SearchAnswer = await fetch(questionURL+'*&facet=genre', {
+            method: 'GET',
+            headers: {
+                'api-key': "D4BD28224DB0862A9819C003C5D90F5B"
+            },
+            body:'',
+            })
+            .then(function (response) {
+                return response.json();
+            });
+    return Promise.resolve(SA["@search.facets"]);
+}
 // Listen for incoming requests 
 server.post('/api/messages', (req, res) => {
     // Route received request to adapter for processing
@@ -167,7 +230,6 @@ server.post('/api/messages', (req, res) => {
         const state = conversationState.get(context);
         const dc = dialogs.createContext(context, state);
         if (isWelcome(context)) {
-            state.menuFlag = true;
             await context.sendActivity(welcomeMessage);
             await dc.begin('MainMenuDialog');
         }
@@ -248,6 +310,7 @@ dialogs.add('FAQDialog', [
             await dc.context.sendActivity("Sorry to hear that! I'll use your feedback to better answer your questions in the future!");
         }
         addFAQ(state.questionText, state.answerText, state.correctAnswer);
+        refreshBot(state);
         await dc.end();
         await dc.begin('MainMenuDialog');
     }
@@ -267,7 +330,10 @@ dialogs.add('MainMenuDialog', [
                 await dc.end();
                 await dc.begin('BSDialog');    
             }
-
+            else if (results === 'Navigate') {
+                await dc.end();
+                await dc.begin('NavDialog');
+            }
         }
     }
 ]);
@@ -276,7 +342,7 @@ dialogs.add('BSDialog', [
     async function (dc) {
         await dc.prompt('textPrompt', "What band would you like to search for?");
     },
-    async function (dc, results) {
+    async function (dc, results :string ) {
         //Search and find the count to post plural response
         var searchResult : Band[] = await bandSearch(results);
         var state = conversationState.get(dc.context);
@@ -287,21 +353,40 @@ dialogs.add('BSDialog', [
                 CardFactory.heroCard(
                     searchResult[0].bandName,
                     `${searchResult[0].day}, ${searchResult[0].startTime} at the ${searchResult[0].stage} Stage`,
-                    [searchResult[0].imageUrl],
-                    ['Description','Back to Main Menu']
+                    [await findPictureURL(searchResult[0].imageUrl)],
+                    [{
+                        type: ActionTypes.ImBack,
+                        title:'Description',
+                        value: 'Description'
+                    },
+                    {
+                        type: ActionTypes.ImBack,
+                        title:'Back to Main Menu',
+                        value: "Back to Main Menu"
+                    }]
                 )
             );
             await dc.context.sendActivity(message);
         }
         else if (searchResult.length > 1) {
+            await dc.context.sendActivity("Here are the shows you are looking for!");
             var l : number = searchResult.length;
             var list = [];
             for (var i : number = 0; i < l; i++) {
                 list[i] = CardFactory.heroCard(
                     searchResult[i].bandName,
                     `${searchResult[i].day}, ${searchResult[i].startTime} at the ${searchResult[i].stage} Stage`,
-                    [searchResult[i].imageUrl],
-                    ['Description','Back to Main Menu']
+                    [await findPictureURL(searchResult[i].imageUrl)],
+                    [{
+                        type: ActionTypes.PostBack,
+                        title: 'Description',
+                        value: `${i}: Description`,
+                    },
+                    {
+                        type: ActionTypes.ImBack,
+                        title: 'Back to Main Menu',
+                        value: 'Back to Main Menu' 
+                    }]
                 )
             }
             //caroussel
@@ -309,23 +394,138 @@ dialogs.add('BSDialog', [
         }
         else {
             await dc.context.sendActivity("There is no band by that title!");
-            state.currentBands = [];
+            refreshBot(state);
             dc.end();
             dc.begin("MainMenuDialog")
         }
     },
     async function (dc, result) {
         var state = conversationState.get(dc.context);
-        if (result === 'Description') {
-            console.log("Hello");
-            await dc.context.sendActivity(state.currentBands[0].description);
+        if (result === 'Back to Main Menu') {
+            refreshBot(state);
+            await dc.end();
+            await dc.begin('MainMenuDialog');            
         }
-        state.currentBands = [];
-        await dc.end();
-        await dc.begin('MainMenuDialog');
+        else if (result === 'Description') {
+            await dc.context.sendActivity(state.currentBands[0].description);
+            state.currentBands = [];
+            await dc.end();
+            await dc.begin('MainMenuDialog');
+        }
+        else {
+            var first : string = result[0];
+            await dc.context.sendActivity(state.currentBands[first].description);
+            refreshBot(state);
+            await dc.end();
+            await dc.begin('MainMenuDialog');
+        }        
     }
 ]);
 
+dialogs.add('NavDialog', [
+    async function(dc) {
+        var facetResults : DGFacets = await daySearch();
+        var days = facetResults.day;
+        var list : string[] = [];
+        for (var i = 0; i < days.length; i++) {
+            list[i] = days[i].value;
+        }
+        list[list.length] = 'Any';
+        const dayMessage = MessageFactory.suggestedActions(list, 'What day would you like to see music?');
+        await dc.context.sendActivity(dayMessage);
+    },
+    async function(dc,response) {
+        var state = conversationState.get(dc.context);
+        //await dc.context.sendActivity(`You said ${response}`);
+        state.navDay = response;
+        var facetResults : DGFacets = await genreSearch();
+        var genres = facetResults.genre;
+        var list : string[] = [];
+        for (var i = 0; i < genres.length; i++) {
+            list[i] = genres[i].value;
+        }
+        list[list.length] = 'Any';
+        var dayQuestion : string;
+        if (state.navDay === 'Any') {
+            dayQuestion = 'What genre of music would you like to see?';
+        }
+        else {
+            dayQuestion = `What genre of music would you like to see on ${state.navDay}?`;
+        }
+        const genreMessage = MessageFactory.suggestedActions(list, dayQuestion);
+        await dc.context.sendActivity(genreMessage);
+    },
+    async function (dc, result) {
+        var state = conversationState.get(dc.context);
+        state.navGenre = result;
+        //Search and find the count to post plural response
+        var searchResult : Band[] = await navSearch(state.navDay, state.navGenre);
+        var state = conversationState.get(dc.context);
+        state.currentBands = searchResult;
+        //Create message
+        var pluralShow: string = searchResult.length > 1 ? 'shows' : 'show';
+        var pluralIs : string = searchResult.length > 1 ? 'are' : 'is';
+        var totalMessage : string;
+        if (state.navDay === 'Any' && state.navGenre === 'Any') {
+            totalMessage = `Here ${pluralIs} the ${pluralShow}!`
+        }
+        else if (state.navDay === 'Any') {
+            totalMessage = `Here ${pluralIs} ${state.navGenre} the ${pluralShow}!`
+        }
+        else if (state.navGenre === 'Any') {
+            totalMessage = `Here ${pluralIs} the ${pluralShow} on ${state.navDay}!`
+        }
+        else {
+            totalMessage = `Here ${pluralIs} the ${state.navGenre} ${pluralShow} on ${state.navDay}!`;
+        }
+        if (searchResult.length >= 1) {
+            await dc.context.sendActivity(totalMessage);
+            var l : number = searchResult.length;
+            var list = [];
+            for (var i : number = 0; i < l; i++) {
+                list[i] = CardFactory.heroCard(
+                    searchResult[i].bandName,
+                    `${searchResult[i].day}, ${searchResult[i].startTime} at the ${searchResult[i].stage} Stage`,
+                    [await findPictureURL(searchResult[i].imageUrl)],
+                    [{
+                        type: ActionTypes.PostBack,
+                        title: 'Description',
+                        value: `${i}: Description`,
+                    },
+                    {
+                        type: ActionTypes.ImBack,
+                        title: 'Back to Main Menu',
+                        value: 'Back to Main Menu' 
+                    }]
+                )
+            }
+            //caroussel
+            await dc.context.sendActivity(MessageFactory.carousel(list));
+        }
+        else {
+            await dc.context.sendActivity("There is no bands with that genre on that day!");
+            state.currentBands = [];
+            refreshBot(state);
+            dc.end();
+            dc.begin("MainMenuDialog")
+        }
+    },
+    async function (dc, result) {
+        var state = conversationState.get(dc.context);
+        if (result === 'Back to Main Menu') {
+            await dc.end();
+            await dc.begin('MainMenuDialog');            
+        }
+        else {
+            var first : string = result[0];
+            await dc.context.sendActivity(state.currentBands[first].description);
+            state.currentBands = [];
+            refreshBot(state);
+            await dc.end();
+            await dc.begin('MainMenuDialog');
+        }        
+    }
+])
 
 dialogs.add('textPrompt', new botbuilder_dialogs.TextPrompt());
 dialogs.add('choicePrompt', new botbuilder_dialogs.ChoicePrompt());
